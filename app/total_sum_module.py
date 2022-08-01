8fafd61497df370625f1cb27094e751392f2fbdd
Maxat Akbanov <30049303+Brain2life@users.noreply.github.com>
2022-08-01 13:45:37 +0600

    [file]: Add test files

--- app/total_sum_module.py
+++ app/total_sum_module.py
@@ -5,4 +5,13 @@ from typing import List
 
 def total(xs: List[float]) -> float:
     """Total returns the sum of xs"""
-    return -1.0
+    result: float = 0.0
+    # For each x float in xs, add it to result
+    for x in xs:
+        result += x
+    return result
+
+
+def join(xs: List[int], delimeter: str) -> str:
+    """Produce a string where subsequent items are separated by delimeter"""
+    return delimeter.join([str(x) for x in xs])
--- app/total_sum_module_test.py
+++ app/total_sum_module_test.py
@@ -1,7 +1,33 @@
 """An example of a test module"""
 
-from app.total_sum_module import total
+from app.total_sum_module import join, total
 
 
 def test_total_empty() -> None:
+    """Total of empty list should be 0.0"""
     assert total([]) == 0.0
+
+
+def test_total_single_item() -> None:
+    """Total of a single item list should be the first item's value"""
+    assert total([110.0]) == 110.0
+
+
+def test_total_many_items() -> None:
+    """Total of a list with many items should be their sum"""
+    assert total([1.0, 2.0, 3.0]) == 6.0
+
+
+def test_join_use_case() -> None:
+    """Join of a list of integers with separator should be a string separated by delimeter"""
+    assert join([1, 2, 3], ", ") == "1, 2, 3"
+
+
+def test_join_edge_single_item() -> None:
+    """Join of a single element should return a single string element without separator"""
+    assert join([1], ", ") == "1"
+
+
+def test_join_edge_empty_delimeter() -> None:
+    """Join of a list of integers with empty list, should return a string of integers joined without delimeter"""
+    assert join([1, 2, 3], "") == "123"
